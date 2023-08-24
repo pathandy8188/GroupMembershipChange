45f151a8c142b6511ce7f0d2b4d1bfd34c6ff4bc
Patrick Handy <75041275+pathandy8188@users.noreply.github.com>
2023-08-24 15:28:11 -0700

    Update GroupMembershipChange.ps1

--- GroupMembershipChange.ps1
+++ GroupMembershipChange.ps1
@@ -1,45 +1,47 @@
-﻿$GroupName = “Domain Admins”
-$Groupfile = "C:\scripts\$GroupName.txt"
-$Groupfilenew = "C:\scripts\$GroupName-new.txt"
-$date=Get-Date -Format F
-$smtpserver = "smtpserver"
-$fromemail = "alerts@domainname.com"
-$toemail = "rcptname@domainname.com"
-#check if Groupfile exists, if not create one
-if (-not(Test-Path -Path $Groupfile -PathType Leaf)) {
-  try {
-    (Get-ADGroupMember -Identity $GroupName).Name | Out-File $Groupfile
+﻿#array of GroupNames
+$GroupNames = (“Domain Admins”, "Enterprise Admins"
+foreach ($GroupName in $GroupNames){
+  $Groupfile = "C:\scripts\$GroupName.txt"
+  $Groupfilenew = "C:\scripts\$GroupName-new.txt"
+  $date=Get-Date -Format F
+  $smtpserver = "mail.smtp2go.com"
+  $fromemail = "alerts@domainname.com"
+  $toemail = "rcptname@domainname.com"
+  #check if Groupfile exists, if not create one
+  if (-not(Test-Path -Path $Groupfile -PathType Leaf)) {
+    try {
+      (Get-ADGroupMember -Identity $GroupName).Name | Out-File $Groupfile
     }
     catch {
-    throw $_.Exception.Message
+      throw $_.Exception.Message
     }
-}
-#write contents of Members to new file to compare
-(Get-ADGroupMember -Identity $GroupName).Name | Out-File $Groupfilenew
-#Get contents of both files
-$GroupFileContent = Get-Content $Groupfile
-$GroupFilenewContent = Get-Content $Groupfilenew
-
-$resultadd=(Compare-Object -ReferenceObject $Groupfilecontent -DifferenceObject $Groupfilenewcontent | Where-Object {$_.SideIndicator -eq "=>"} | Select-Object -ExpandProperty InputObject) -join ", "
-If ($resultadd) 
-{
-#Someone was added to the Group $_.SideIndicator -eq "=>"
-$body = "<font face=""verdana"">
-<p>This alert was generated at $date</p>
-<p>$resultadd was added to the group $GroupName<br /> 
-</font>"
-Send-MailMessage -From $fromemail -To $toemail -SmtpServer $smtpserver -port 2525 -Subject "$GroupName Membership Changed | $result was added to the Group $GroupName" -Body $body -BodyAsHtml -Priority High
-(Get-ADGroupMember -Identity $GroupName).Name | Out-File $Groupfile
-}
-$resultremove=(Compare-Object -ReferenceObject $Groupfilecontent -DifferenceObject $Groupfilenewcontent | Where-Object {$_.SideIndicator -eq "<="} | Select-Object -ExpandProperty InputObject) -join ", "
-If ($resultremove) 
-{
-#Someone was removed from the Group $_.SideIndicator -eq "<="
-$body = "<font face=""verdana"">
-<p>This alert was generated at $date</p>
-<p>$resultremove was removed from the group $GroupName<br /> 
-</font>"
-Send-MailMessage -From $fromemail -To $toemail -SmtpServer $smtpserver -port 2525 -Subject "$Groupname Membership Changed | $result was removed from the Group $GroupName" -Body $body -BodyAsHtml -Priority High
-#Set new Group Members to compare
-(Get-ADGroupMember -Identity $GroupName).Name | Out-File $Groupfile
-}
+  }
+  #write contents of Members to new file to compare
+  (Get-ADGroupMember -Identity $GroupName).Name | Out-File $Groupfilenew
+  #Get contents of both files
+  $GroupFileContent = Get-Content $Groupfile
+  $GroupFilenewContent = Get-Content $Groupfilenew
+  #Compare to see if member was added
+  $resultadd=(Compare-Object -ReferenceObject $Groupfilecontent -DifferenceObject $Groupfilenewcontent | Where-Object {$_.SideIndicator -eq "=>"} | Select-Object -ExpandProperty InputObject) -join ", "
+  If ($resultadd) {
+  #Someone was added to the Group $_.SideIndicator -eq "=>"
+  $body = "<font face=""verdana"">
+  <p>This alert was generated at $date</p>
+  <p>$resultadd was added to the group $GroupName<br /> 
+  </font>"
+  Send-MailMessage -From $fromemail -To $toemail -SmtpServer $smtpserver -port 2525 -Subject "$GroupName Membership Changed | $result was added to the Group $GroupName" -Body $body -BodyAsHtml -Priority High
+  (Get-ADGroupMember -Identity $GroupName).Name | Out-File $Groupfile
+  } End if resultadd
+  #Compare to see if member was removed
+  $resultremove=(Compare-Object -ReferenceObject $Groupfilecontent -DifferenceObject $Groupfilenewcontent | Where-Object {$_.SideIndicator -eq "<="} | Select-Object -ExpandProperty InputObject) -join ", "
+  If ($resultremove) {
+  #Someone was removed from the Group $_.SideIndicator -eq "<="
+  $body = "<font face=""verdana"">
+  <p>This alert was generated at $date</p>
+  <p>$resultremove was removed from the group $GroupName<br /> 
+  </font>"
+  Send-MailMessage -From $fromemail -To $toemail -SmtpServer $smtpserver -port 2525 -Subject "$Groupname Membership Changed | $result was removed from the Group $GroupName" -Body $body -BodyAsHtml -Priority High
+  #Set new Group Members to compare
+  (Get-ADGroupMember -Identity $GroupName).Name | Out-File $Groupfile
+  } #endif Resultremove
+} #End For Each GroupName in GroupNames
